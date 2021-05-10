26f566bea572cfb640eabc47a674a5c283d0e52d
italomlp <italomlp@gmail.com>
2021-05-10 01:11:04 -0300

    feat: make useInfinityFetch hook more powerful

--- src/hooks/useInfinityFetch.ts
+++ src/hooks/useInfinityFetch.ts
@@ -1,12 +1,44 @@
 import { useSWRInfinite } from 'swr';
+import { KeyLoader, SWRInfiniteConfiguration } from 'swr/dist/types';
+import { AxiosResponse } from 'axios';
+import { useState } from 'react';
+
 import api from '../services/api';
 
-export function useInfinityFetch<Data = unknown, Error = unknown>(
-  getKey: Parameters<typeof useSWRInfinite>[0],
-) {
-  return useSWRInfinite<Data, Error>(getKey, async (receivedUrl) => {
-    const response = await api.get(receivedUrl);
+export type GetDataFromInfinityFetchResponseFunctionType<
+  Response = unknown,
+  Data = unknown
+> = (data: AxiosResponse<Response>) => { data: Data; reachedEnd: boolean };
 
-    return response.data;
-  });
+export function useInfinityFetch<
+  Data = unknown,
+  Error = unknown,
+  Response = unknown
+>(
+  getKey: KeyLoader<Data>,
+  getDataFromResponse?: GetDataFromInfinityFetchResponseFunctionType<
+    Response,
+    Data
+  >,
+  swrOptions?: SWRInfiniteConfiguration<Data, Error>,
+) {
+  const [isReachEnd, setIsReachEnd] = useState(false);
+
+  const swrResponse = useSWRInfinite<Data, Error>(
+    getKey,
+    async (receivedUrl) => {
+      const response = await api.get(receivedUrl);
+
+      if (getDataFromResponse) {
+        const { data: parsedData, reachedEnd } = getDataFromResponse(response);
+        setIsReachEnd(reachedEnd);
+        return parsedData;
+      }
+
+      return response.data;
+    },
+    swrOptions,
+  );
+
+  return { ...swrResponse, isReachEnd };
 }
